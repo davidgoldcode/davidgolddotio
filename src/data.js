10bba71f9ad7e2c68123f29076051208921c8f24
David Gold <davidgold@hey.com>
2021-01-11 22:56:05 -0500

    added still version of calc

--- src/data.js
+++ src/data.js
@@ -2,6 +2,8 @@ import { default as rotationsvg } from "./assets/original/therotation.svg";
 import { default as rotationwebp } from "./assets/webp/therotation.webp";
 import { default as deathmetalgif } from "./assets/original/deathmetal.gif";
 import { default as deathmetalwebp } from "./assets/webp/deathmetal.webp";
+import { default as deathmetalstillsvg } from "./assets/webp/deathmetalstill.webp";
+import { default as deathmetalstillwebp } from "./assets/webp/deathmetalstill.webp";
 
 export const skills = [
   "python",
@@ -39,9 +41,15 @@ export const projects = [
     body:
       "The simple calculator: a rite of passage in a new developer’s portfolio but with a hardcore twist 🤘",
     tech: ["React", "Material UI", "Create React App"],
+    gif: {
+      src: deathmetalgif,
+      fallback: deathmetalwebp,
+      alt: "Exploding Calculator",
+      class: "object-none w-full bg-black",
+    },
     img: {
-      src: deathmetalwebp,
-      fallback: deathmetalgif,
+      src: deathmetalstillwebp,
+      fallback: deathmetalstillwebp,
       alt: "Exploding Calculator",
       class: "object-none w-full bg-black",
     },
