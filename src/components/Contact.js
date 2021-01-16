4c3e86812571b396ed96ba35fca7c33d062b41d0
David Gold <davidgold@hey.com>
2021-01-16 16:06:12 -0500

    Much more responsive on mobile

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,36 +8,36 @@ import {
 import GitHubCalendar from "react-github-calendar";
 
 const Contact = () => {
-  const iconSize = "4rem";
-
   return (
-    <main className="flex flex-col items-center justify-around h-screen w-screen">
-      <h1 className="text-4xl font-black uppercase p-4">
-        See other projects on Github
-      </h1>
-      <GitHubCalendar username="davidgoldcode" />
-      <div className="flex flex-col items-center h-1/3">
-        <h1 className="text-4xl font-black uppercase">Get in touch</h1>
-        <p className="text-xl font-black ">Project idea?</p>
-        <p className="text-xl font-black ">Business inquiry?</p>
-        <p className="text-xl font-black ">Dad joke?</p>
-        <p className="text-xl ">Reach out below 👇 and say hello </p>
+    <main className="flex flex-col items-center text-center justify-around h-screen w-screen">
+      <h1>See other projects on Github</h1>
+      <div className="md: w-9/12">
+        <GitHubCalendar username="davidgoldcode" />
+      </div>
+      <div className="flex flex-col items-center h-1/3 w-9/12">
+        <h1>Get in touch</h1>
+        <p className="md:text-xl text-base font-black ">Project idea?</p>
+        <p className="md:text-xl text-base font-black ">Business inquiry?</p>
+        <p className="md:text-xl text-base font-black ">Dad joke?</p>
+        <p className="md:text-xl text-base ">
+          Reach out below 👇 and say hello{" "}
+        </p>
       </div>
       <section className="flex items-center justify-around w-3/5">
         <a href="https://github.com/davidgoldcode">
-          <AiFillGithub style={{ fontSize: iconSize }} />
+          <AiFillGithub />
         </a>
         <a href="mailto:davidgold@hey.com">
-          <AiOutlineMail style={{ fontSize: iconSize }} />
+          <AiOutlineMail />
         </a>
         <a href="https://twitter.com/davidigold">
-          <AiOutlineTwitter style={{ fontSize: iconSize }} />
+          <AiOutlineTwitter />
         </a>
         <a href="https://golddavid.medium.com">
-          <AiFillMediumSquare style={{ fontSize: iconSize }} />
+          <AiFillMediumSquare />
         </a>
         <a href="https://www.linkedin.com/in/davidigold/">
-          <AiFillLinkedin style={{ fontSize: iconSize }} />
+          <AiFillLinkedin />
         </a>
       </section>
     </main>
